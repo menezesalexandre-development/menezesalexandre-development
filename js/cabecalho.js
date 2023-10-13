4c0cc88253a7eed84185ac83d22819be03392563
menezesalexandre_dev <alexandremmnzssilva2509@gmail.com>
2023-10-13 16:43:55 -0300

    COMMIT - 2023/10/13

--- js/cabecalho.js
+++ js/cabecalho.js
@@ -1,30 +1,33 @@
 //CONSTANTE - CABEÇALHO:
 const cabecalho = `<div class="title">
-  <a href="index.html" target="_self"><h2><</h2></a>
-  <a href="index.html" target="_self"><h1>menezesalexandre_dev</h1></a>
-  <a href="index.html" target="_self"><h2>></h2></a>
+  <a href="../index.html" target="_self"><h2><</h2></a>
+  <a href="../index.html" target="_self"><h1>menezesalexandre_dev</h1></a>
+  <a href="../index.html" target="_self"><h2>></h2></a>
 </div>
 <div class="social">
   <ul>
     <li>
-      <a
+      <a 
+        id="social_github"
         href="https://github.com/menezesalexandre-development"
         target="_blank"
-        ><img src="img/icons/bxl-github.png" alt="GitHub"
+        ><img src="../img/icons/bxl-github.png" alt="GitHub"
       /></a>
     </li>
     <li>
       <a
+        id="social_linkedin"
         href="https://www.linkedin.com/in/alexandre-m-menezes-silva-836191289/"
         target="_blank"
-        ><img src="img/icons/bxl-linkedin-square.png" alt="LinkedIn"
+        ><img src="../img/icons/bxl-linkedin-square.png" alt="LinkedIn"
       /></a>
     </li>
     <li>
       <a
+        id="social_upwork"
         href="https://www.upwork.com/freelancers/~018421fe5c76db8eb8"
         target="_blank"
-        ><img src="img/icons/bxl-upwork.png" alt="Upwork"
+        ><img src="../img/icons/bxl-upwork.png" alt="Upwork"
       /></a>
     </li>
   </ul>
--- js/rodape.js
+++ js/rodape.js
@@ -5,21 +5,21 @@ const rodape = `<div class="social">
     <a
       href="https://github.com/menezesalexandre-development"
       target="_blank"
-      ><img src="img/icons/bxl-github.png" alt="GitHub"
+      ><img src="../img/icons/bxl-github.png" alt="GitHub"
     /></a>
   </li>
   <li>
     <a
       href="https://www.linkedin.com/in/alexandre-m-menezes-silva-836191289/"
       target="_blank"
-      ><img src="img/icons/bxl-linkedin-square.png" alt="LinkedIn"
+      ><img src="../img/icons/bxl-linkedin-square.png" alt="LinkedIn"
     /></a>
   </li>
   <li>
     <a
       href="https://www.upwork.com/freelancers/~018421fe5c76db8eb8"
       target="_blank"
-      ><img src="img/icons/bxl-upwork.png" alt="Upwork"
+      ><img src="../img/icons/bxl-upwork.png" alt="Upwork"
     /></a>
   </li>
 </ul>
