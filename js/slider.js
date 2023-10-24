6ccbadf68cbb8adf8bbcf0716a787eb57be85681
menezesalexandre_dev <alexandremmnzssilva2509@gmail.com>
2023-10-24 19:42:35 -0300

    COMMIT2 - 2023/10/24

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 1800,
+      disableOnInteraction: false,
+    },
+    
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
